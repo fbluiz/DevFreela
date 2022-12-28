8c4c073b2deb9f673b49c7203b1395be5c77e676
Fabio Luiz <fbluiz.sf@gmail.com>
2022-12-28 16:28:31 -0300

    Teste unitario Projects (xUnit)

---
+++ iDevApi.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,34 @@
+﻿using iDev.Core.Entities;
+using iDev.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace iDevApi.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ProjectStartWorks()
+        {
+            var project = new Project("Nome de Teste", "Descricao de Teste", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
