e5cde912beb0b0af07adb1a9e7b2372dadfdd9ea
Fabio Luiz <fbluiz.sf@gmail.com>
2022-12-28 19:56:42 -0300

    Teste unnitário GetAllProjectsCommandHandler

---
+++ iDevApi.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using iDev.Application.Queries.GetAllProjects;
+using iDev.Core.Entities;
+using iDev.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace iDevApi.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
+        {
+            // Arange
+            var projects = new List<Project>
+            {
+                new Project("Nome Do Teste 1", "Descricao De Teste 1", 1,2, 10000),
+                new Project("Nome Do Teste 2", "Descricao De Teste 2", 1,2, 20000),
+                new Project("Nome Do Teste 3", "Descricao De Teste 3", 1,2, 30000),
+            };
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjectsQuery = new GetAllProjectsQuery("");
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+
+            // Act
+            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList);
+            Assert.Equal(projects.Count, projectViewModelList.Count);
+
+            projectRepositoryMock.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+        }
+
+    }
+}
